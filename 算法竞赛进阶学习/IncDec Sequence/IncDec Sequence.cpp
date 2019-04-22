44222fdf43bb32100a8009679539fd9d07b1b120
43EVER <i@43ever.me>
2019-04-22 15:35:42 +0800

    算法进阶学习第7天

---
+++ 算法竞赛进阶学习/IncDec Sequence/IncDec Sequence.cpp
@@ -0,0 +1,36 @@
+﻿// IncDec Sequence.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 1e6 + 5;
+long long a[MAX];
+long long b[MAX];
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+	long long p = 0, q = 0;
+	for (int i = 1; i <= n; i++) {
+		b[i] = a[i] - a[i - 1];
+		if (i > 1 && b[i] > 0) p += b[i];
+		else if (i > 1 && b[i] < 0) q += b[i];
+	}
+	cout << max(p, q) << endl;
+	cout << abs(p - q) + 1 << endl;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
