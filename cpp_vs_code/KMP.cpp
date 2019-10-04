a856a58678df27ba95ae1a63de3a85b8e2744598
43EVER <i@43ever.me>
2019-10-04 23:00:47 +0800

    字符串学习

---
+++ cpp_vs_code/KMP.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string a;
+
+    scanf("%s", a.c_str() + 1);
+
+    cout << a.c_str() + 1 << endl;
+}
